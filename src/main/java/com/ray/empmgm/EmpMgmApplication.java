45a0f3dfe3c57a8b669f91203c67b8a5bb7931c7
Rakesh Kymar Ray <rakeshrayk@gmail.com>
2021-10-19 12:38:54 +0530

    project push

---
+++ src/main/java/com/ray/empmgm/EmpMgmApplication.java
@@ -0,0 +1,13 @@
+package com.ray.empmgm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpMgmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpMgmApplication.class, args);
+	}
+
+}
